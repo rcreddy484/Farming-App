50884e6bd25968e6efcf0ea4e3f6c4745c57b0b7
rcreddy484 <rcreddy484@gmail.com>
2023-12-05 22:54:20 +0530

    parameter 3 added in prepamentFragment file

--- app/src/main/java/com/project/farmingapp/PrePaymentFragment.kt
+++ app/src/main/java/com/project/farmingapp/PrePaymentFragment.kt
@@ -19,6 +19,7 @@ import org.json.JSONObject
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM3 = "param3"
 
 /**
  * A simple [Fragment] subclass.
